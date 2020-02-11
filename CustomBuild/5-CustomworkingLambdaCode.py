c6a3de4414be20140fcc173978caf93db4265e96
Matheus Arrais <mlarrais@amazon.com>
2020-02-11 15:56:17 -0300

    adding new files

---
+++ CustomBuild/5-CustomworkingLambdaCode.py
@@ -0,0 +1,114 @@
+from __future__ import print_function
+import boto3
+import logging
+import json
+import time
+import cfnresponse
+import os
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+def get_client(service):
+  client = boto3.client(service)
+  return client
+
+def get_template(sourcebucket,accountsandboxtemplate):
+    
+
+    '''
+        Read a template file and return the contents
+    '''
+    #print("Reading resources from " + templatefile)
+    s3 = boto3.resource('s3')
+    #obj = s3.Object('cf-to-create-lambda','5-newbaseline.yml')
+    obj = s3.Object(sourcebucket,accountsandboxtemplate)
+    return obj.get()['Body'].read().decode('utf-8') 
+
+def deploy_resources(template, stackname, stackregion, accountid, executionrole, administrationrole, accountemail, accountdays):
+
+    '''
+        Create a CloudFormation stack of resources within the new account
+    '''
+
+    datestamp = time.strftime("%d/%m/%Y")
+    client = boto3.client('cloudformation',region_name=stackregion)
+    #print("Creating stack " + stackname + " in " + account_id)
+
+
+    create_stack_response = client.create_stack_set(
+                StackSetName=stackname,
+                TemplateBody=template,
+                Parameters=[
+                    {
+                        'ParameterKey' : 'DaysForAccount',
+                        'ParameterValue' : accountdays
+                    },
+                    {
+                        'ParameterKey' : 'AddrEmail',
+                        'ParameterValue' : accountemail
+                    },
+                    {
+                        'ParameterKey' : 'AccountIdSandbox',
+                        'ParameterValue' : accountid
+                    }
+                ],
+                Capabilities=[
+                    'CAPABILITY_NAMED_IAM',
+                ],
+
+                Tags=[
+                    {
+                        'Key': 'ManagedResource',
+                        'Value': 'True'
+                    },
+                    {
+                        'Key': 'DeployDate',
+                        'Value': datestamp
+                    }
+                ],
+                AdministrationRoleARN=administrationrole,
+                ExecutionRoleName=executionrole
+            )
+            
+    response = client.create_stack_instances(
+                StackSetName=stackname,
+                Accounts=[accountid],
+                Regions=[stackregion]
+                )
+
+
+def main(event, context):
+            response_data = {}
+            try:
+                if event["RequestType"] == "Create" or event["RequestType"] == "Update":
+                    print(event)
+                    client = get_client('organizations')
+                    accountid = os.environ['accountid']
+                    accountdays = os.environ['accountdays']
+                    accountemail = os.environ['accountemail']
+                    #accountrole = os.environ['accountrole']
+                    #templatefile = os.environ['templatefile']
+                    stackname = os.environ['stackname']
+                    stackregion = os.environ['stackregion']
+                    administrationrole = os.environ['administrationrole']
+                    executionrole = os.environ['executionrole']
+                    sourcebucket = os.environ['sourcebucket']
+                    accountsandboxtemplate = os.environ['accountsandboxtemplate']
+
+                    template = get_template(sourcebucket,accountsandboxtemplate)
+                    stack = deploy_resources(template, stackname, stackregion, accountid, executionrole, administrationrole, accountemail, accountdays)
+                    print(stack)
+
+                    logger.info("Response - " + json.dumps(response_data))
+                    cfnresponse.send(event, context, cfnresponse.SUCCESS, response_data)
+
+                elif event["RequestType"] == "Delete":
+                    logger.info("Event Body - " + json.dumps(event))
+                    cfnresponse.send(event, context, cfnresponse.SUCCESS,{})
+                else:
+                  logger.info("Event Body - " + json.dumps(event))
+                  cfnresponse.send(event, context, cfnresponse.FAILED,{})
+            except Exception as e:
+                  logger.error(e, exc_info=True)
+                  response_data = {'Error': str(e)}
+                  cfnresponse.send(event, context, cfnresponse.FAILED, response_data)
---
+++ CustomStart/create-scp.py
@@ -0,0 +1,95 @@
+import boto3, json
+import cfnresponse
+import os
+import logging
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+def lambda_handler(event, context):
+            responseData = {}
+            logger.info('event: {}'.format(event))
+            logger.info('context: {}'.format(context))
+            client = boto3.client('organizations')
+            ssm = boto3.client('ssm')
+            logger.info('Always printing the event: {}'.format(event))
+            if event["RequestType"] == "Create" or event["RequestType"] == "Update":
+              try:
+                  logger.info("Event Body - " + json.dumps(event))
+                  
+                  policies = client.list_policies(
+                      Filter='SERVICE_CONTROL_POLICY'
+                    )
+                  policy_name = 'DenyAllOutsideUSandBR'
+                  denypolicyname = [name['Name'] for name in policies['Policies']]
+                  denypolicyid = [id['Id'] for id in policies['Policies']]
+
+                  if policy_name in denypolicyname:
+                    print ("Policy already exists")
+                    for i, (name, id) in enumerate(zip(denypolicyname, denypolicyid)):
+                      if policy_name in name:
+                        parameter = ssm.get_parameter(
+                          Name='account_id'
+                          )
+                        parametervalue = parameter['Parameter']['Value']
+                      
+                        attach = client.attach_policy(
+                            PolicyId=id,
+                            TargetId=parametervalue
+                            )
+                        print("Policy apply in account")
+                  else:
+                    policy = client.create_policy(
+                      Content='{"Version": "2012-10-17","Statement": [{"Sid": "DenyAllOutsideUSandBR","Effect": "Deny","NotAction": ["iam:*","organizations:*","route53:*","budgets:*","waf:*","cloudfront:*","globalaccelerator:*","importexport:*","support:*"],"Resource": "*","Condition": {"StringNotEquals": {"aws:RequestedRegion": ["us-east-1","sa-east-1"]}}}]}',
+                      Description='Denny all services outsite N.Virginia and South America',
+                      Name='DenyAllOutsideUSandBR',
+                      Type='SERVICE_CONTROL_POLICY'
+                      )
+    
+                    policy_id = policy['Policy']['PolicySummary']['Id']
+
+                    create_parameter_policy = ssm.put_parameter(
+                        Name='policy_id',
+                        Value=policy_id,
+                        Type='String',
+                        Overwrite=True
+                    )
+
+                    parameter = ssm.get_parameter(
+                      Name='account_id'
+                      )
+                    id = parameter['Parameter']['Value']
+                  
+                    attach = client.attach_policy(
+                      PolicyId=policy_id,
+                      TargetId=id
+                      )
+                    print ("Policy apply sucessfully in account")
+
+                  cfnresponse.send(event, context, cfnresponse.SUCCESS, responseData, 'CustomResourcePhysicalID')
+              except Exception as e:
+                    logger.error(e, exc_info=True)
+                    responseData = {'Error': str(e)}
+                    cfnresponse.send(event, context, cfnresponse.FAILED, responseData, 'CustomResourcePhysicalID')
+                    
+            if event["RequestType"] == "Delete":
+                try:
+                    parameter_acc_id = ssm.get_parameter(
+                        Name='account_id'
+                      )
+                    parameter_policy = ssm.get_parameter(
+                        Name='policy_id'
+                    )
+
+                    detach = client.detach_policy(
+                      PolicyId=parameter_policy,
+                      TargetId=parameter_acc_id
+                    )
+                    
+                    delete = client.delete_policy(
+                      PolicyId=parameter_policy
+                    )
+                    logger.info("Event Body - " + json.dumps(event))
+                    cfnresponse.send(event, context, cfnresponse.SUCCESS, responseData, 'CustomResourcePhysicalID')
+                except Exception as e:
+                  logger.error(e, exc_info=True)
+                  responseData = {'Error': str(e)}
+                  cfnresponse.send(event, context, cfnresponse.FAILED, responseData, 'CustomResourcePhysicalID')
