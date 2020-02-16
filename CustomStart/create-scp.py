f1fe6426725db7974e89adb88335ef75d60546d1
Matheus Arrais <mlarrais@amazon.com>
2020-02-16 12:17:38 -0300

    Update create-scp

--- CustomStart/create-scp.py
+++ CustomStart/create-scp.py
@@ -18,14 +18,19 @@ def lambda_handler(event, context):
                   policies = client.list_policies(
                       Filter='SERVICE_CONTROL_POLICY'
                     )
-                  policy_name = 'DenyAllOutsideUSandBR'
+                  policy_deny_name = 'DenyAllOutsideUSandBR'
+                  policy_deploy_name = 'ProtectSandboxDeploy'
+
                   denypolicyname = [name['Name'] for name in policies['Policies']]
                   denypolicyid = [id['Id'] for id in policies['Policies']]
 
-                  if policy_name in denypolicyname:
+                  deploypolicyname = [name['Name'] for name in policies['Policies']]
+                  deploypolicyid = [id['Id'] for id in policies['Policies']]
+
+                  if policy_deny_name in denypolicyname:
                     print ("Policy already exists")
                     for i, (name, id) in enumerate(zip(denypolicyname, denypolicyid)):
-                      if policy_name in name:
+                      if policy_deny_name in name:
                       
                         attach = client.attach_policy(
                             PolicyId=id,
@@ -33,17 +38,42 @@ def lambda_handler(event, context):
                             )
                         print("Policy apply in account")
                   else:
-                    policy = client.create_policy(
+                    policy_deny_resources = client.create_policy(
                       Content='{"Version": "2012-10-17","Statement": [{"Sid": "DenyAllOutsideUSandBR","Effect": "Deny","NotAction": ["iam:*","organizations:*","route53:*","budgets:*","waf:*","cloudfront:*","globalaccelerator:*","importexport:*","support:*"],"Resource": "*","Condition": {"StringNotEquals": {"aws:RequestedRegion": ["us-east-1","sa-east-1"]}}}]}',
                       Description='Denny all services outsite N.Virginia and South America',
                       Name='DenyAllOutsideUSandBR',
                       Type='SERVICE_CONTROL_POLICY'
                       )
-    
-                    policy_id = policy['Policy']['PolicySummary']['Id']
+
+                    policy_id_deny_resources = policy_deny_resources['Policy']['PolicySummary']['Id']
+
+                    attach_deny_resources = client.attach_policy(
+                      PolicyId=policy_id_deny_resources,
+                      TargetId=accountid
+                      )
                   
-                    attach = client.attach_policy(
-                      PolicyId=policy_id,
+                  if  polpolicy_deploy_nameicy_name in denypolicyname:
+                    print("Policy already exists")
+                    for i, (name, id) in enumerate(zip(denypolicyname, denypolicyid)):
+                      if policy_deploy_name in name:
+                      
+                        attach = client.attach_policy(
+                            PolicyId=id,
+                            TargetId=accountid
+                            )
+                        print("Policy apply in account")
+                  else:  
+                    policy_deploy_resources = client.create_policy(
+                      Content='{"Version":"2012-10-17","Statement":[{"Sid":"ProtectStepFunction","Effect":"Deny","Action":["states:*"],"Resource":["arn:aws:states:*:*:stateMachine:StepFunctionMonitoring"],"Condition":{"ArnNotLike":{"aws:PrincipalARN":["arn:aws:iam::*:role/RoleForStepFunctionSandboxAccount","arn:aws:iam::*:role/RoleForLambdaFullAccess"]}}},{"Sid":"ProtectCodeBuild","Effect":"Deny","Action":["codebuild:*"],"Resource":["arn:aws:codebuild:*:*:project/AccountNuker-List","arn:aws:codebuild:*:*:project/AccountNuker-Delete"],"Condition":{"ArnNotLike":{"aws:PrincipalARN":["arn:aws:iam::*:role/RoleForCodeBuildSandbox","arn:aws:iam::*:role/RoleForLambdaFullAccess"]}}},{"Sid":"ProtectSSMParameterStore","Effect":"Deny","Action":["ssm:*"],"Resource":["arn:aws:ssm:*:*:parameter/account_days","arn:aws:ssm:*:*:parameter/account_id"],"Condition":{"ArnNotLike":{"aws:PrincipalARN":["arn:aws:iam::*:role/RoleForLambdaFullAccess","arn:aws:iam::*:role/RoleForCodeBuildSandbox","arn:aws:iam::*:role/RoleForStepFunctionSandboxAccount"]}}}]}',
+                      Description='Protect all services deployed by StackSet Account Sandbox',
+                      Name='ProtectSandboxDeploy',
+                      Type='SERVICE_CONTROL_POLICY'
+                      )
+
+                    policy_id_deploy_resources = policy_deploy_resources['Policy']['PolicySummary']['Id']
+
+                    attach_deploy_resources = client.attach_policy(
+                      PolicyId=policy_id_deploy_resources,
                       TargetId=accountid
                       )
                     print ("Policy apply sucessfully in account")
